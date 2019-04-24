5159fdce3e5c32c6a3abea21d346dbcf6326a8f0
Joli Holmes <jh111@ad.rice.edu>
2019-04-24 10:08:12 -0500

    added additional column level tests

--- R/test_col.R
+++ R/test_col.R
@@ -1,11 +1,146 @@
-test_unique_test <- function() {}
+## General column tests
 
-test_unique <- function() {}
+#' @title Test uniqueness
+#' @description Tests if the vector/column is unique
+#' @param vec the vector or column to test
+#' @return boolean
+#' @examples
+#' vec1 <- c(1,2,3)
+#' vec2 <- c(1,1,2)
+#' 
+#' test_unique_test(vec1)
+#' # Returns TRUE
+#' 
+#' test_unique_test(vec2)
+#' ## Returns FALSE
+test_unique_test <- function(vec) {
+    
+    if (length(unique(vec)) == length(vec)) {
+        return(TRUE)
+    } else {
+        return(FALSE)
+    }
+}
 
-test_values_test <- function (){}
+#' @inherit test_unique_test return title
+#' @description Tests if the vector/column is unique
+#' @inheritParams test_unique_test
+#' @family general column tests
+#' @return vector
+#' @examples
+#' vec1 <- c(1,2,3)
+#' vec2 <- c(1,1,2)
+#' 
+#' test_unique(vec1)[2]
+#' ## Returns PASS
+#' 
+#' test_unique(vec2)[2]
+#' ## Returns ERROR 
+test_unique <- function(vec) {
+    
+    td <- "Unique Values"
+    
+    if (test_unique_test(vec)) {
+        return(c(td, test_pass_ti, test_pass_tm))
+    } else {
+        return(c(td, test_fail_ti, "Not Unique"))
+    }
+    
+}
 
-test_values <- function() {}
+#' @title Test expected values
+#' @description Tests if the vector/column contains values other than expected
+#' @param add_values a list additional values
+#' @return boolean
+#' @examples
+#' 
+#' test_values_test(c())
+#' # Returns TRUE
+#' 
+#' test_values_test(c(4,5))
+#' ## Returns FALSE
+test_values_test <- function(add_values) {
+    
+    if (length(add_values) == 0) {
+        return(TRUE)
+    } else {
+        return(FALSE)
+    }
+    
+}
 
-test_null_values_test <- function() {}
+#' @inherit test_values_test return title
+#' @description Tests if the vector/column contains values other than expected
+#' @inheritParams test_values_test
+#' @family general column tests
+#' @return vector
+#' @examples
+#' vec1 <- c(1,2,3)
+#' vec2 <- c(1,1,2)
+#' expc_values <- c(1, 2)
+#' 
+#' test_values(vec2, expc_values)[2]
+#' ## Returns PASS
+#' 
+#' test_values(vec1, expc_values)[2]
+#' ## Returns ERROR 
+test_values <- function(vec, expc_values) {
+    
+    td <- "Expected Values"
+    add_values <- setdiff(unique(vec), expc_values)
+    
+    if (test_values_test(add_values)) {
+        return(c(td, test_pass_ti, test_pass_tm))
+    } else {
+        return(c(td, test_fail_ti, paste0("Additional values in col", paste(add_values, collapse = ","))))
+    }
+    
+}
 
-test_null_values <- function() {}
+#' @title Test NA values
+#' @description Tests if the vector/column contains any NA values
+#' @param vec the vector or column to test
+#' @return boolean
+#' @examples
+#' vec1 <- c(1,2,3)
+#' vec2 <- c(1,NA,2)
+#' 
+#' test_null_values_test(vec1)
+#' # Returns TRUE
+#' 
+#' test_null_values_test(vec2)
+#' ## Returns FALSE
+test_null_values_test <- function(vec) {
+  
+  if(all(!is.na(vec))) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+  
+}
+
+#' @inherit test_null_values_test return title
+#' @description Tests if the vector/column contains any NA values
+#' @inheritParams test_null_values_test
+#' @family general column tests
+#' @return vector
+#' @examples
+#' vec1 <- c(1,2,3)
+#' vec2 <- c(1,NA,2)
+#' 
+#' test_null_values(vec1)[2]
+#' ## Returns PASS
+#' 
+#' test_null_values(vec2)[2]
+#' ## Returns ERROR 
+test_null_values <- function(vec) {
+    
+    td <- "Null Values"
+    
+    if (test_null_values_test(vec)) {
+        return(c(td, test_pass_ti, test_pass_tm))
+    } else {
+        return(c(td, test_fail_ti, "Contains null values"))
+    }
+}
---
+++ tests/testthat/test_test_col.R
@@ -0,0 +1,50 @@
+vec1 <- c(1, 2, 3)
+vec2 <- c(1, 1, 2)
+vec3 <- c(1, NA, 2)
+
+testthat::test_that("tests test_unique_test evaluates correctly", {
+    
+    testthat::expect_true(test_unique_test(vec1))
+    testthat::expect_false(test_unique_test(vec2))
+    
+})
+
+testthat::test_that("tests test_unique evaluates correctly", {
+    
+    testthat::expect_equal(length(test_unique(vec1)), 3)
+    testthat::expect_equal(test_unique(vec1)[2], test_pass_ti)
+    
+    testthat::expect_equal(length(test_unique(vec2)), 3)
+    testthat::expect_equal(test_unique(vec2)[2], test_fail_ti)
+    
+})
+
+
+testthat::test_that("tests test_values_test evaluates correctly", {
+    
+    expc_values <- c(1, 2)
+    
+    testthat::expect_equal(length(test_values(vec2, expc_values)), 3)
+    testthat::expect_equal(test_values(vec2, expc_values)[2], test_pass_ti)
+    
+    testthat::expect_equal(length(test_values(vec1, expc_values)), 3)
+    testthat::expect_equal(test_values(vec1, expc_values)[2], test_fail_ti)
+    
+})
+
+testthat::test_that("tests test_null_values_test evaluates correctly", {
+    
+    testthat::expect_true(test_null_values_test(vec1))
+    testthat::expect_false(test_null_values_test(vec3))
+    
+})
+
+testthat::test_that("tests test_null_values evaluates correctly", {
+    
+    testthat::expect_equal(length(test_null_values(vec1)), 3)
+    testthat::expect_equal(test_null_values(vec1)[2], test_pass_ti)
+    
+    testthat::expect_equal(length(test_null_values(vec3)), 3)
+    testthat::expect_equal(test_null_values(vec3)[2], test_fail_ti)
+    
+})
