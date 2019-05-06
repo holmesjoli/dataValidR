2e760b26cdc4f3c53cef0bb40f5fb193dc46226d
Joli Holmes <jh111@ad.rice.edu>
2019-05-06 15:42:18 -0500

    added test function

--- R/test_utils.R
+++ R/test_utils.R
@@ -3,3 +3,14 @@ test_fail_ti <- "ERROR"
 test_warn_ti <- "WARNING"
 
 test_pass_tm <- ""
+
+
+named_df <- function(x) {
+  
+  df <- data.frame(matrix(ncol = 3, nrow = 0))
+  names(df) <- c("Description", "Indicator", "Message")
+  names(x) <- c("Description", "Indicator", "Message")
+  df <- rbind(df, x)
+  
+  return(df)
+}
