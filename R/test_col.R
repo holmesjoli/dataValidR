83bcc6a0e640e4e6af38adb79e578803fec467fc
Joli Holmes <jh111@ad.rice.edu>
2019-07-19 10:00:29 -0500

    reparameterized functions to simplify logic

--- R/test_col.R
+++ R/test_col.R
@@ -1,74 +1,50 @@
-## General column tests
-
 #' @title Test uniqueness
 #' @description Tests if the vector/column is unique
-#' @param vec the vector or column to test
-#' @return boolean
+#' @param df the dataframe
+#' @inheritParams class_test_unique
 #' @examples
 #' \dontrun{
-#' vec1 <- c(1,2,3)
-#' vec2 <- c(1,1,2)
+#' df <- data.frame(x = 1:4, y = c(1,1,2:3))
 #' 
-#' test_unique_test(vec1)
-#' # Returns TRUE
+#' test <- test_unique_test(df, "x")
+#' ## test$test_result returns TRUE
 #' 
-#' test_unique_test(vec2)
-#' ## Returns FALSE
+#' test <- test_unique_test(df, "y)
+#' ## test$test_result returns FALSE
 #' }
-test_unique_test <- function(vec) {
+test_unique <- function(df, col_name) {
     
-    if (length(unique(vec)) == length(vec)) {
-        return(TRUE)
+  class <- class_test_unique(col_name)
+  
+    if (length(unique(df[[class$col_name]])) == length(df[[class$col_name]])) {
+        
+      class$test_result <- TRUE
+      class$test_message <- "PASSED"
+      
     } else {
-        return(FALSE)
+      class$test_result <- FALSE
+      class$test_message <- "FAILED: Not Unique"
     }
-}
-
-#' @inherit test_unique_test return title
-#' @description Tests if the vector/column is unique
-#' @inheritParams test_unique_test
-#' @family general column tests
-#' @return vector
-#' @examples
-#' vec1 <- c(1,2,3)
-#' vec2 <- c(1,1,2)
-#' 
-#' test_unique(vec1)[2]
-#' ## Returns PASS
-#' 
-#' test_unique(vec2)[2]
-#' ## Returns ERROR 
-#' @export
-test_unique <- function(vec) {
-    
-    td <- "Unique Values"
-    
-    if (test_unique_test(vec)) {
-        return(c(td, test_pass$ti, test_pass$tm))
-    } else {
-        return(c(td, test_fail$ti, "Not Unique"))
-    }
-    
+  
+  return(class)
 }
 
 #' @title Test expected values
 #' @description Tests if the vector/column contains values other than expected
 #' @param df the dataframe
 #' @inheritParams class_test_values
-#' @return boolean
+#' @return class with test_result, test_message, test_description
 #' @examples
 #' \dontrun{
 #' df <- data.frame(x = 1:4, y = 5:8)
-#' col_name <- "x"
 #' values <- 1:4
 #' na <- FALSE
 #' 
-#' test <- test_values(df, col_name, values, na)
-#' ## Returns TRUE
+#' test <- test_values(df, "x", values, na)
+#' ## test$test_result returns TRUE
 #' 
-#' col_name <- "y"
-#' test <- test_values(df, col_name, values, na)
-#' ## Returns FALSE
+#' test <- test_values(df, "y", values, na)
+#' ## test$test_result returns FALSE
 #' }
 test_values <- function(df, col_name, values, na) {
   
@@ -96,51 +72,32 @@ test_values <- function(df, col_name, values, na) {
 
 #' @title Test NA values
 #' @description Tests if the vector/column contains any NA values
-#' @param vec the vector or column to test
-#' @return boolean
+#' @param df the dataframe
+#' @inheritParams class_test_na
 #' @examples
 #' \dontrun{
-#' vec1 <- c(1,2,3)
-#' vec2 <- c(1,NA,2)
+#' df <- data.frame(x = 1:4, y = c(NA, 6:8))
 #' 
-#' test_null_values_test(vec1)
-#' # Returns TRUE
+#' test_na(df, "x")
+#' ## test$test_result returns TRUE
 #' 
-#' test_null_values_test(vec2)
-#' ## Returns FALSE
+#' test_na(df, "y")
+#' ## test$test_result returns FALSE
 #' }
-test_null_values_test <- function(vec) {
+test_na <- function(df, col_name) {
     
-    if (all(!is.na(vec))) {
-        return(TRUE)
+  class <- class_test_na(col_name)
+  
+    if (all(!is.na(df[[class$col_name]]))) {
+        
+      class$test_result <- TRUE
+      class$test_message <- "PASSED"
+      
     } else {
-        return(FALSE)
+      
+      class$test_result <- FALSE  
+      class$test_message <- "FAILED: Contains null values"
     }
     
-}
-
-#' @inherit test_null_values_test return title
-#' @description Tests if the vector/column contains any NA values
-#' @inheritParams test_null_values_test
-#' @family general column tests
-#' @return vector
-#' @examples
-#' vec1 <- c(1,2,3)
-#' vec2 <- c(1,NA,2)
-#' 
-#' test_null_values(vec1)[2]
-#' ## Returns PASS
-#' 
-#' test_null_values(vec2)[2]
-#' ## Returns ERROR
-#' @export
-test_null_values <- function(vec) {
-    
-    td <- "Null Values"
-    
-    if (test_null_values_test(vec)) {
-        return(c(td, test_pass$ti, test_pass$tm))
-    } else {
-        return(c(td, test_fail$ti, "Contains null values"))
-    }
+  return(class)
 }
