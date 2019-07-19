6836bfb99d69b4a7ed3e17aed0a1e0f7f34e9a70
Joli Holmes <jh111@ad.rice.edu>
2019-07-19 10:56:28 -0500

    refactored code

--- tests/testthat/test_test_numeric_col.R
+++ tests/testthat/test_test_numeric_col.R
@@ -1,166 +1,27 @@
-# Tests for numeric column tests
+testthat::test_that("Min NA", { 
 
-vec <- c(1:3)
-vec2 <- c(1:5)
-df <- data.frame(col = vec)
+  col <- c(1:3)
+  
+  testthat::expect_equal(min_na(col, TRUE), 1)
+  testthat::expect_equal(min_na(col, FALSE), 1)
 
-testthat::test_that("tests test_less_than_value_test evaluates correctly", {
-    
-    testthat::expect_true(test_less_than_value_test(vec, 4))
-    testthat::expect_false(test_less_than_value_test(vec, 3))
-    
+  col <- c(1,3, NA)  
+  
+  testthat::expect_equal(min_na(col, TRUE), 1)
+  testthat::expect_true(is.na(min_na(col, FALSE)))
+  
 })
 
-testthat::test_that("tests test_less_than_value evaluates correctly", {
-    
-    testthat::expect_equal(length(test_less_than_value(vec, 4)), 3)
-    testthat::expect_equal(test_less_than_value(vec, 4)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_less_than_value(vec, 3)), 3)
-    testthat::expect_equal(test_less_than_value(vec, 3)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_less_than_or_equal_value_test evaluates correctly", {
-    
-    testthat::expect_true(test_less_than_or_equal_value_test(vec, 4))
-    testthat::expect_true(test_less_than_or_equal_value_test(vec, 3))
-    testthat::expect_false(test_less_than_or_equal_value_test(vec, 2))
-    
-})
-
-testthat::test_that("tests test_less_than_or_equal_value evaluates correctly", {
-    
-    testthat::expect_equal(length(test_less_than_or_equal_value(vec, 4)), 3)
-    testthat::expect_equal(test_less_than_or_equal_value(vec, 4)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_less_than_or_equal_value(vec, 3)), 3)
-    testthat::expect_equal(test_less_than_or_equal_value(vec, 3)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_less_than_or_equal_value(vec, 2)), 3)
-    testthat::expect_equal(test_less_than_or_equal_value(vec, 2)[2], test_fail$ti)
-    
-})
-
-
-testthat::test_that("tests test_greater_than_value_test evaluates correctly", {
-    
-    testthat::expect_true(test_greater_than_value_test(vec, 0))
-    testthat::expect_false(test_greater_than_value_test(vec, 1))
-    
-})
-
-testthat::test_that("tests test_greater_than_value evaluates correctly", {
-    
-    testthat::expect_equal(length(test_greater_than_value(vec, 0)), 3)
-    testthat::expect_equal(test_greater_than_value(vec, 0)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_greater_than_value(vec, 1)), 3)
-    testthat::expect_equal(test_greater_than_value(vec, 1)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_greater_than_or_equal_value_test evaluates correctly", {
-    
-    testthat::expect_true(test_greater_than_or_equal_value_test(vec, 0))
-    testthat::expect_true(test_greater_than_or_equal_value_test(vec, 1))
-    testthat::expect_false(test_greater_than_or_equal_value_test(vec, 2))
-    
-})
-
-testthat::test_that("tests test_greater_than_or_equal_value evaluates correctly", {
-    
-    testthat::expect_equal(length(test_greater_than_or_equal_value(vec, 0)), 3)
-    testthat::expect_equal(test_greater_than_or_equal_value(vec, 0)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_greater_than_or_equal_value(vec, 1)), 3)
-    testthat::expect_equal(test_greater_than_or_equal_value(vec, 1)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_greater_than_or_equal_value(vec, 2)), 3)
-    testthat::expect_equal(test_greater_than_or_equal_value(vec, 2)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_exclu_value_range_test evaluates correctly", {
-    
-    testthat::expect_true(test_exclu_value_range_test(vec, 0, 4))
-    testthat::expect_false(test_exclu_value_range_test(vec, 0, 3))
-    testthat::expect_false(test_exclu_value_range_test(vec, 1, 3))
-    
-})
-
-testthat::test_that("tests test_greater_than_or_equal_value evaluates correctly", {
-    
-    testthat::expect_equal(length(test_exclu_value_range(vec, 0, 4)), 3)
-    testthat::expect_equal(test_exclu_value_range(vec, 0, 4)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_exclu_value_range(vec, 0, 3)), 3)
-    testthat::expect_equal(test_exclu_value_range(vec, 0, 3)[2], test_fail$ti)
-    
-    testthat::expect_equal(length(test_exclu_value_range(vec, 1, 3)), 3)
-    testthat::expect_equal(test_exclu_value_range(vec, 1, 3)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_inclu_value_range evaluates correctly", {
-    
-    testthat::expect_true(test_inclu_value_range_test(vec, 1, 3))
-    testthat::expect_true(test_inclu_value_range_test(vec, 1, 4))
-    testthat::expect_false(test_inclu_value_range_test(vec, 2, 5))
-    
-})
-
-testthat::test_that("tests test_inclu_value_range evaluates correctly", {
-    
-    testthat::expect_equal(length(test_inclu_value_range(vec, 1, 3)), 3)
-    testthat::expect_equal(test_inclu_value_range(vec, 1, 3)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_inclu_value_range(vec, 1, 4)), 3)
-    testthat::expect_equal(test_inclu_value_range(vec, 1, 4)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_inclu_value_range(vec, 2, 5)), 3)
-    testthat::expect_equal(test_inclu_value_range(vec, 2, 5)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_exclu_lower_inclu_upper_range_test evaluates correctly", {
-    
-    testthat::expect_true(test_exclu_lower_inclu_upper_range_test(vec, 0, 3))
-    testthat::expect_false(test_exclu_lower_inclu_upper_range_test(vec, 1, 3))
-    testthat::expect_false(test_exclu_lower_inclu_upper_range_test(vec, 0, 2))
-    
-})
-
-testthat::test_that("tests test_exclu_lower_inclu_upper_range evaluates correctly", {
-    
-    testthat::expect_equal(length(test_exclu_lower_inclu_upper_range(vec, 0, 3)), 3)
-    testthat::expect_equal(test_exclu_lower_inclu_upper_range(vec, 0, 3)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_exclu_lower_inclu_upper_range(vec, 1, 3)), 3)
-    testthat::expect_equal(test_exclu_lower_inclu_upper_range(vec, 1, 3)[2], test_fail$ti)
-    
-    testthat::expect_equal(length(test_exclu_lower_inclu_upper_range(vec, 0, 2)), 3)
-    testthat::expect_equal(test_exclu_lower_inclu_upper_range(vec, 0, 2)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_inclu_lower_exclu_upper_range_test evaluates correctly", {
-    
-    testthat::expect_true(test_inclu_lower_exclu_upper_range_test(vec, 0, 4))
-    testthat::expect_false(test_inclu_lower_exclu_upper_range_test(vec, 1, 3))
-    testthat::expect_false(test_inclu_lower_exclu_upper_range_test(vec, 0, 2))
-    
-})
-
-testthat::test_that("tests test_inclu_lower_exclu_upper_range_test evaluates correctly", {
-    
-    testthat::expect_equal(length(test_inclu_lower_exclu_upper_range(vec, 0, 4)), 3)
-    testthat::expect_equal(test_inclu_lower_exclu_upper_range(vec, 0, 4)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_inclu_lower_exclu_upper_range(vec, 1, 3)), 3)
-    testthat::expect_equal(test_inclu_lower_exclu_upper_range(vec, 1, 3)[2], test_fail$ti)
-    
-    testthat::expect_equal(length(test_inclu_lower_exclu_upper_range(vec, 0, 2)), 3)
-    testthat::expect_equal(test_inclu_lower_exclu_upper_range(vec, 0, 2)[2], test_fail$ti)
-    
+testthat::test_that("Max NA", { 
+  
+  col <- c(1:3)
+  
+  testthat::expect_equal(max_na(col, TRUE), 3)
+  testthat::expect_equal(max_na(col, FALSE), 3)
+  
+  col <- c(1,3, NA)  
+  
+  testthat::expect_equal(max_na(col, TRUE), 3)
+  testthat::expect_true(is.na(max_na(col, FALSE)))
+  
 })
