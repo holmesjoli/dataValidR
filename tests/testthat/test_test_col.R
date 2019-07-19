5c9f969f388671f85219537125725aa82913deff
Joli Holmes <jh111@ad.rice.edu>
2019-07-19 09:41:13 -0500

    refactored functions and refactored tests

--- tests/testthat/test_test_col.R
+++ tests/testthat/test_test_col.R
@@ -1,50 +1,16 @@
-vec1 <- c(1, 2, 3)
-vec2 <- c(1, 1, 2)
-vec3 <- c(1, NA, 2)
-
-testthat::test_that("tests test_unique_test evaluates correctly", {
-    
-    testthat::expect_true(test_unique_test(vec1))
-    testthat::expect_false(test_unique_test(vec2))
-    
-})
-
-testthat::test_that("tests test_unique evaluates correctly", {
-    
-    testthat::expect_equal(length(test_unique(vec1)), 3)
-    testthat::expect_equal(test_unique(vec1)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_unique(vec2)), 3)
-    testthat::expect_equal(test_unique(vec2)[2], test_fail$ti)
-    
-})
-
-
-testthat::test_that("tests test_values_test evaluates correctly", {
-    
-    expc_values <- c(1, 2)
-    
-    testthat::expect_equal(length(test_values(vec2, expc_values)), 3)
-    testthat::expect_equal(test_values(vec2, expc_values)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_values(vec1, expc_values)), 3)
-    testthat::expect_equal(test_values(vec1, expc_values)[2], test_fail$ti)
-    
-})
-
-testthat::test_that("tests test_null_values_test evaluates correctly", {
-    
-    testthat::expect_true(test_null_values_test(vec1))
-    testthat::expect_false(test_null_values_test(vec3))
-    
-})
-
-testthat::test_that("tests test_null_values evaluates correctly", {
-    
-    testthat::expect_equal(length(test_null_values(vec1)), 3)
-    testthat::expect_equal(test_null_values(vec1)[2], test_pass$ti)
-    
-    testthat::expect_equal(length(test_null_values(vec3)), 3)
-    testthat::expect_equal(test_null_values(vec3)[2], test_fail$ti)
-    
+testthat::test_that("test_values returns TRUE when PASSING, FALSE when FAILING", {
+  
+  df <- data.frame(x = 1:4, y = 5:8)
+  col_name <- "x"; values <- 1:4; na <- FALSE
+   
+  test <- test_values(df, col_name, values, na)
+  testthat::expect_true(test$test_result)
+  testthat::expect_that(test$test_result, testthat::equals(TRUE))
+  
+  col_name <- "y"
+  
+  test <- test_values(df, col_name, values, na)
+  testthat::expect_false(test$test_result)
+  testthat::expect_that(test$test_result, testthat::equals(FALSE))
+  
 })
