6cbed583051e4ee3ea97a5a3c70485dd326a2126
Joli Holmes <jh111@ad.rice.edu>
2019-07-23 12:25:44 -0500

    reparameterized functions

--- tests/testthat/test_setup_test_classes.R
+++ tests/testthat/test_setup_test_classes.R
@@ -3,231 +3,129 @@ testthat::test_that("is_int returns TRUE for integers", {
   testthat::expect_true(is_int(1))
   testthat::expect_false(is_int(1.0))
   testthat::expect_false(is_int(1.1))
+  testthat::expect_false(is_int(TRUE))
+  testthat::expect_false(is_int(FALSE))
   testthat::expect_true(all(is_int(c(1,2,3))))
   testthat::expect_false(all(is_int(c(1,2,3.5))))
   
 })
 
-testthat::test_that("class_test_numeric_range : upper_inclu and lower_inclu have to be boolean", {
+testthat::test_that("expc_params_check", {
   
-  df_name <- "x"; col_name <- "x"; 
-  upper <- 5; lower <- 3
-  upper_inclu <- TRUE; lower_inclu <- TRUE; na = TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper_inclu <- TRUE; lower_inclu <- FALSE
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper_inclu <- FALSE; lower_inclu <- TRUE
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper_inclu <- FALSE; lower_inclu <- FALSE
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper_inclu <- FALSE; lower_inclu <- "X"
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                            upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper_inclu <- TRUE; lower_inclu <- "X"
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper_inclu <- "X"; lower_inclu <- TRUE
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper_inclu <- "X"; lower_inclu <- FALSE
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-
-})
-
-testthat::test_that("class_test_numeric_range : either upper_inclu or lower_inclu must not be NULL", {
-  
-  df_name <- "x"; col_name <- "x"; 
-  upper <- NULL; lower <- 3
-  upper_inclu <- NULL; lower_inclu <- TRUE; na = TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper <- NULL; lower <- 3
-  upper_inclu <- NULL; lower_inclu <- FALSE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper <- 3; lower <- NULL
-  upper_inclu <- TRUE; lower_inclu <- NULL
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper <- 3; lower <- NULL
-  upper_inclu <- FALSE; lower_inclu <- NULL
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper <- NULL; lower <- NULL
-  upper_inclu <- NULL; lower_inclu <- NULL
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper <- 3; lower <- NULL
-  upper_inclu <- NULL; lower_inclu <- NULL
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper <- NULL; lower <- NULL
-  upper_inclu <- TRUE; lower_inclu <- NULL
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper <- NULL; lower <- NULL
-  upper_inclu <- NULL; lower_inclu <- FALSE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper <- NULL; lower <- 1
-  upper_inclu <- NULL; lower_inclu <- NULL
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
+  expc_params <- c("df_name", "col_name", "na")
+  cls <- list(df_name = "x", col_name = "x")
+  testthat::expect_error(test_expc_params(cls, expc_params))
   
 })
 
-testthat::test_that("class_test_numeric  upper and lower are numeric or NULL", {
+testthat::test_that("test_param_logical", {
   
-  df_name <- "x"; col_name <- "x"; 
-  upper <- NULL; lower <- 3
-  upper_inclu <- NULL; lower_inclu <- TRUE; na = TRUE;
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  upper <- 5; lower <- 3
-  upper_inclu <- TRUE; lower_inclu <- TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("numeric"))
-  
-  
-  upper <- 5; lower <- "X"
-  upper_inclu <- TRUE; lower_inclu <- TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  
-  upper <- "X"; lower <- 3
-  upper_inclu <- TRUE; lower_inclu <- TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper <- c(1,2); lower <- 3
-  upper_inclu <- TRUE; lower_inclu <- TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
-  
-  upper <- 3; lower <- c(1,2)
-  upper_inclu <- TRUE; lower_inclu <- TRUE
-  
-  testthat::expect_that(class_test_numeric_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
+  cls <- list(t = NULL, u = NA, v = FALSE, w = TRUE, x = 1.1, y = 1, z = "X")
+  testthat::expect_error(test_param_logical(cls, "t"))
+  testthat::expect_error(test_param_logical(cls, "u"))
+  testthat::expect_silent(test_param_logical(cls, "v"))
+  testthat::expect_silent(test_param_logical(cls, "w"))
+  testthat::expect_error(test_param_logical(cls, "x"))
+  testthat::expect_error(test_param_logical(cls, "y"))
+  testthat::expect_error(test_param_logical(cls, "z"))
   
 })
 
-testthat::test_that("class_test_integer: test only takes integers", {
+testthat::test_that("test_param_logical_or_null", {
   
-  col_name <- "x"; upper <- NULL; lower <- 3
-  upper_inclu <- NULL; lower_inclu <- TRUE; na = TRUE;
+  cls <- list(t = NULL, u = NA, v = FALSE, w = TRUE, x = 1.1, y = 1, z = "X")
+  testthat::expect_silent(test_param_logical_or_null(cls, "t"))
+  testthat::expect_error(test_param_logical_or_null(cls, "u"))
+  testthat::expect_silent(test_param_logical_or_null(cls, "v"))
+  testthat::expect_silent(test_param_logical_or_null(cls, "w"))
+  testthat::expect_error(test_param_logical_or_null(cls, "z"))
+  testthat::expect_error(test_param_logical_or_null(cls, "y"))
+
+})
+
+testthat::test_that("test_param_numeric", {
   
-  testthat::expect_that(class_test_integer_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("integer"))
+  cls <- list(t = NULL, u = NA, v = FALSE, w = TRUE, x = 1.1, y = 1, z = "X")
+  testthat::expect_error(test_param_numeric(cls, "t"))
+  testthat::expect_error(test_param_numeric(cls, "u"))
+  testthat::expect_error(test_param_numeric(cls, "v"))
+  testthat::expect_error(test_param_numeric(cls, "w"))
+  testthat::expect_silent(test_param_numeric(cls, "x"))
+  testthat::expect_silent(test_param_numeric(cls, "y"))
+  testthat::expect_error(test_param_numeric(cls, "z"))
   
-  upper <- 3; lower <- NULL
-  upper_inclu <- TRUE; lower_inclu <- NULL
+})
+
+testthat::test_that("test_param_numeric_or_null", {
   
-  testthat::expect_that(class_test_integer_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::is_a("integer"))
+  cls <- list(t = NULL, u = NA, v = FALSE, w = TRUE, x = 1.1, y = 1, z = "X")
+  testthat::expect_error(test_param_numeric_or_null(cls, "u"))
+  testthat::expect_error(test_param_numeric_or_null(cls, "v"))
+  testthat::expect_error(test_param_numeric_or_null(cls, "w"))
+  testthat::expect_silent(test_param_numeric_or_null(cls, "x"))
+  testthat::expect_silent(test_param_numeric_or_null(cls, "y"))
+  testthat::expect_error(test_param_numeric_or_null(cls, "z"))
   
-  upper <- 3.5; lower <- NULL
-  upper_inclu <- TRUE; lower_inclu <- NULL
+})
+
+testthat::test_that("test_param_integer_or_null", {
   
-  testthat::expect_that(class_test_integer_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
+  cls <- list(t = NULL, u = NA, v = FALSE, w = TRUE, x = 1.1, y = 1, z = "X")
+  testthat::expect_error(test_param_integer_or_null(cls, "u"))
+  testthat::expect_error(test_param_integer_or_null(cls, "v"))
+  testthat::expect_error(test_param_integer_or_null(cls, "w"))
+  testthat::expect_error(test_param_integer_or_null(cls, "x"))
+  testthat::expect_silent(test_param_integer_or_null(cls, "y"))
+  testthat::expect_error(test_param_integer_or_null(cls, "z"))
   
-  upper <- NULL; lower <- 3.5
-  upper_inclu <- NULL; lower_inclu <- FALSE
+})
+
+testthat::test_that("test_param_string", {
   
-  testthat::expect_that(class_test_integer_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
+  cls <- list(t = NULL, u = NA, v = FALSE, w = TRUE, x = 1.1, y = 1, z = "X")
+  testthat::expect_error(test_param_string(cls, "t"))
+  testthat::expect_error(test_param_string(cls, "u"))
+  testthat::expect_error(test_param_string(cls, "v"))
+  testthat::expect_error(test_param_string(cls, "w"))
+  testthat::expect_error(test_param_string(cls, "x"))
+  testthat::expect_silent(test_param_string(cls, "z"))
   
-  upper <- 7.5; lower <- 3.5
-  upper_inclu <- TRUE; lower_inclu <- FALSE
+})
+
+testthat::test_that("test_params_both_null_or_not", {
   
-  testthat::expect_that(class_test_integer_range(df_name, col_name, 
-                                           upper_inclu, lower_inclu, upper, lower, na), 
-                        testthat::throws_error())
+  cls <- list(x = NULL, y = "z")
+  testthat::expect_error(test_params_both_null_or_not(cls, "x", "y"))
+  testthat::expect_silent(test_params_both_null_or_not(cls, "x", "x"))
+  testthat::expect_silent(test_params_both_null_or_not(cls, "y", "y"))
+  
+})
+
+testthat::test_that("test_params_both_not_null", {
+  
+  cls <- list(x = NULL, y = "z")
+  testthat::expect_error(test_params_both_not_null(cls, "x", "x"))
+  testthat::expect_silent(test_params_both_not_null(cls, "x", "y"))
+  testthat::expect_silent(test_params_both_not_null(cls, "y", "x"))
+  testthat::expect_silent(test_params_both_not_null(cls, "y", "y"))
   
 })
 
 testthat::test_that("class_test_values tests", {
   
-  col_name <- "X"; values <- c(1,2,3); na <- NA
+  cls <- list(df_name = "x", col_name = "X", 
+              values = c(1,2,3), na = NA)
   
-  testthat::expect_that(class_test_values(df_name, col_name, values, na), 
-                        testthat::throws_error())
+  testthat::expect_error(class_test_values(cls))
   
-  col_name <- "X"; values <- c(1,2,3); na <- TRUE
+  cls <- list(df_name = "x", col_name = "X", 
+              values = c(1,2,3), na = TRUE)
   
-  values <- class_test_values(df_name, col_name, values, na)$values
+  testthat::expect_true(NA %in% class_test_values(cls)$values)
   
-  testthat::expect_true(NA %in% values)
+  cls <- list(df_name = "x", col_name = "X", 
+              values = c(1,2,3), na = FALSE)
   
-  col_name <- "X"; values <- c(1,2,3); na <- FALSE
-  
-  values <- class_test_values(df_name, col_name, values, na)$values
-  
-  testthat::expect_false(NA %in% values)
+  testthat::expect_false(NA %in% class_test_values(cls)$values)
   
 })
