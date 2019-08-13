ea8d06b5808be520ec63c1236350c5b0ea9a50e7
Joli Holmes <jh111@ad.rice.edu>
2019-08-13 11:09:56 -0500

    added merge tets

--- R/test_utils.R
+++ R/test_utils.R
@@ -18,14 +18,26 @@ test_row <- function(test) {
   
 }
 
+test_merge <- function(test){
+  
+  data.frame(test_category = test$test_category,
+             df_left_name = df_left_name,
+             df_right_name = df_right_name,
+             test_name = test$test_name,
+             test_desc = test$test_desc,
+             test_result = test$test_result,
+             test_message = test$test_message)
+}
+
+
 #' @title Apply test
 #' @export
-apply_test <- function(setup, df){
+apply_test <- function(df, setup, df){
   UseMethod("apply_test")
   
 }
 
-apply_test.default <- function(setup, df, ...){
+apply_test.default <- function(df, setup, ...){
   
   warning(paste("apply_test does not know how to handle object of class ", 
                 class(setup),
@@ -35,7 +47,7 @@ apply_test.default <- function(setup, df, ...){
 
 #' @title Tests for NAs
 #' @export
-apply_test.test_na <- function(setup, df) {
+apply_test.test_na <- function(df, setup) {
   
   test <- test_na(df, setup)
   test_result <- test_row(test)
@@ -45,7 +57,7 @@ apply_test.test_na <- function(setup, df) {
 
 #' @title Tests for uniqueness
 #' @export
-apply_test.test_unique <- function(setup, df) {
+apply_test.test_unique <- function(df, setup) {
   
   test <- test_unique(df, setup)
   test_result <- test_row(test)
@@ -65,10 +77,45 @@ apply_test.test_values <- function(setup, df) {
 
 #' @title Tests Range of Values
 #' @export
-apply_test.test_range <- function(setup, df) {
+apply_test.test_range <- function(df, setup) {
   
   test <- test_range(df, setup)
   test_result <- test_row(test)
+  
+  return(test_result)
+}
+
+#' @title Tests One to Many
+#' @param df_left left dataset
+#' @param df_right right dataset
+#' @param setup from the merge setup class
+#' @export
+apply_test.test_one_many <- function(df_left, df_right, setup) {
+  
+  test <- test_one_many(df_left, df_right, setup)
+  test_result <- test_merge(test)
+  
+  return(test_result)
+}
+
+#' @title Tests One to One
+#' @inheritParams apply_test.test_one_many
+#' @export
+apply_test.test_one_one <- function(df_left, df_right, setup) {
+  
+  test <- test_one_one(df_left, df_right, setup)
+  test_result <- test_merge(test)
+  
+  return(test_result)
+}
+
+#' @title Tests One to One
+#' @inheritParams apply_test.test_one_many
+#' @export
+apply_test.test_many_many <- function(df_left, df_right, setup) {
+  
+  test <- test_many_many(df_left, df_right, setup)
+  test_result <- test_merge(test)
   
   return(test_result)
 }
