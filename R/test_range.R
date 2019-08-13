99668834b12c0df6b257b0cb82f6fb9f4cd2e157
Joli Holmes <jh111@ad.rice.edu>
2019-08-13 10:38:09 -0500

    replaced cls with setup to be more consistent with example

--- R/test_range.R
+++ R/test_range.R
@@ -31,207 +31,207 @@ max_na <- function(col, na) {
 #' @title Test Exclu lower
 #' @description Tests that column is greater than the lower bound
 #' @param df the dataframe
-#' @param cls the setup class for testing
+#' @param setup the setup class for testing
 #' @export
-test_exclu_lower <- function(df, cls) {
+test_exclu_lower <- function(df, setup) {
   
-  mn <- min_na(df[[cls$col_name]], cls$na)
+  mn <- min_na(df[[setup$col_name]], setup$na)
   
-  cls$test_desc <- paste0("Test ", cls$col_name," > ", mn)
-  class(cls) <- append(class(cls), "exclu_lower")
+  setup$test_desc <- paste0("Test ", setup$col_name," > ", mn)
+  class(setup) <- append(class(setup), "exclu_lower")
   
   if (!is.na(mn)) {
   
-    if (mn > cls$lower) {
-      cls$test_result <- TRUE
-      cls$test_message <- "PASSED"
+    if (mn > setup$lower) {
+      setup$test_result <- TRUE
+      setup$test_message <- "PASSED"
     } else {
-      cls$test_result <- FALSE
-      cls$test_message <- paste0("FAILED: lower bound is ", mn, " but expected less than ", cls$lower)
+      setup$test_result <- FALSE
+      setup$test_message <- paste0("FAILED: lower bound is ", mn, " but expected less than ", setup$lower)
     }
   } else {
-    cls$test_result <- FALSE
-    cls$test_message <- "NAs present but NA was set to FALSE"
+    setup$test_result <- FALSE
+    setup$test_message <- "NAs present but NA was set to FALSE"
   }
-  return(cls)
+  return(setup)
 }
 
 #' @title Test inclu lower
 #' @description Tests that column is greater than or equal to the lower bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_inclu_lower <- function(df, cls) {
+test_inclu_lower <- function(df, setup) {
   
-  mn <- min_na(df[[cls$col_name]], cls$na)
+  mn <- min_na(df[[setup$col_name]], setup$na)
   
-  cls$test_desc <- paste0("Test ", cls$col_name," >= ", mn)
-  class(cls) <- append(class(cls), "inclu_lower")
+  setup$test_desc <- paste0("Test ", setup$col_name," >= ", mn)
+  class(setup) <- append(class(setup), "inclu_lower")
   
   if (!is.na(mn)) {
     
-    if (mn >= cls$lower) {
-      cls$test_result <- TRUE
-      cls$test_message <- "PASSED"
+    if (mn >= setup$lower) {
+      setup$test_result <- TRUE
+      setup$test_message <- "PASSED"
     } else {
-      cls$test_result <- FALSE
-      cls$test_message <- paste0("FAILED: lower bound is ", mn, " but expected less than or equal to", cls$lower)
+      setup$test_result <- FALSE
+      setup$test_message <- paste0("FAILED: lower bound is ", mn, " but expected less than or equal to", setup$lower)
     }
   } else {
-    cls$test_result <- FALSE
-    cls$test_message <- "NAs present but NA was set to FALSE"
+    setup$test_result <- FALSE
+    setup$test_message <- "NAs present but NA was set to FALSE"
   }
-  return(cls)
+  return(setup)
 }
 
 #' @title Test Exclu upper
 #' @description Tests that column is less than the upper bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_exclu_upper <- function(df, cls) {
+test_exclu_upper <- function(df, setup) {
   
-  mx <- max_na(df[[cls$col_name]], cls$na)
+  mx <- max_na(df[[setup$col_name]], setup$na)
   
-  cls$test_desc <- paste0("Test ", cls$col_name," < ", mx)
+  setup$test_desc <- paste0("Test ", setup$col_name," < ", mx)
   
-  class(cls) <- append(class(cls), "exclu_upper")
+  class(setup) <- append(class(setup), "exclu_upper")
   
   if (!is.na(mx)) {
     
-    if (mx < cls$upper) {
-      cls$test_result <- TRUE
-      cls$test_message <- "PASSED"
+    if (mx < setup$upper) {
+      setup$test_result <- TRUE
+      setup$test_message <- "PASSED"
     } else {
-      cls$test_result <- FALSE
-      cls$test_message <- paste0("FAILED: upper bound is ", mx, " but expected greater than ", cls$upper)
+      setup$test_result <- FALSE
+      setup$test_message <- paste0("FAILED: upper bound is ", mx, " but expected greater than ", setup$upper)
     }
   } else {
-    cls$test_result <- FALSE
-    cls$test_message <- "NAs present but NA was set to FALSE"
+    setup$test_result <- FALSE
+    setup$test_message <- "NAs present but NA was set to FALSE"
   }
-  return(cls)
+  return(setup)
 }
 
 #' @title Test Exclu upper
 #' @description Tests that column is less than or equal to the upper bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_inclu_upper <- function(df, cls) {
+test_inclu_upper <- function(df, setup) {
   
-  mx <- max_na(df[[cls$col_name]], cls$na)
+  mx <- max_na(df[[setup$col_name]], setup$na)
   
-  cls$test_desc <- paste0("Test ", cls$col_name," <= ", mx)
-  class(cls) <- append(class(cls), "inclu_upper")
+  setup$test_desc <- paste0("Test ", setup$col_name," <= ", mx)
+  class(setup) <- append(class(setup), "inclu_upper")
   
   if (!is.na(mx)) {
     
-    if (mx <= cls$upper) {
-      cls$test_result <- TRUE
-      cls$test_message <- "PASSED"
+    if (mx <= setup$upper) {
+      setup$test_result <- TRUE
+      setup$test_message <- "PASSED"
     } else {
-      cls$test_result <- FALSE
-      cls$test_message <- paste0("FAILED: upper bound is ", mx, " but expected greater than or equal to ", cls$upper)
+      setup$test_result <- FALSE
+      setup$test_message <- paste0("FAILED: upper bound is ", mx, " but expected greater than or equal to ", setup$upper)
     }
   } else {
-    cls$test_result <- FALSE
-    cls$test_message <- "NAs present but NA was set to FALSE"
+    setup$test_result <- FALSE
+    setup$test_message <- "NAs present but NA was set to FALSE"
   }
-  return(cls)
+  return(setup)
 }
 
 #' @title Test exclu lower and exclu upper
 #' @description Tests that the column is greater than the lower bound AND less than the upper bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_exclu_lower_exclu_upper <- function(df, cls) {
+test_exclu_lower_exclu_upper <- function(df, setup) {
 
-  el <- test_exclu_lower(df, cls)
-  eu <- test_exclu_upper(df, cls)
+  el <- test_exclu_lower(df, setup)
+  eu <- test_exclu_upper(df, setup)
   
-  cls$test_desc <- paste0("Test ", el$lower," < ", cls$col_name," < ", eu$upper)
-  class(cls) <- append(class(cls), "exclu_lower_exclu_upper")
+  setup$test_desc <- paste0("Test ", el$lower," < ", setup$col_name," < ", eu$upper)
+  class(setup) <- append(class(setup), "exclu_lower_exclu_upper")
   
   if (el$test_result & eu$test_result) {
     
-    cls$test_result <- TRUE
-    cls$test_message <- "PASSED"
+    setup$test_result <- TRUE
+    setup$test_message <- "PASSED"
   
   } else {
     
-    cls$test_result <- FALSE
+    setup$test_result <- FALSE
     
     if (el$test_result) {
-      cls$test_message <- paste0("lower bound: ", el$test_message, ", but upper bound ", eu$test_message)
+      setup$test_message <- paste0("lower bound: ", el$test_message, ", but upper bound ", eu$test_message)
     } else if (eu$test_result) {
-      cls$test_message <- paste0("upper bound: ", eu$test_message, ", but lower bound ", el$test_message)
+      setup$test_message <- paste0("upper bound: ", eu$test_message, ", but lower bound ", el$test_message)
     } else {
-      cls$test_message <- paste0("lower bound: ", el$test_message, " and upper bound: ", eu$test_message)
+      setup$test_message <- paste0("lower bound: ", el$test_message, " and upper bound: ", eu$test_message)
     }
   }
-  return(cls)
+  return(setup)
 }
 
 #' @title Test inclu lower and exclu upper
 #' @description Tests that the column is greater than or equal to the lower bound AND less than the upper bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_inclu_lower_exclu_upper <- function(df, cls) {
+test_inclu_lower_exclu_upper <- function(df, setup) {
   
-  il <- test_inclu_lower(df, cls)
-  eu <- test_exclu_upper(df, cls)
+  il <- test_inclu_lower(df, setup)
+  eu <- test_exclu_upper(df, setup)
   
-  cls$test_desc <- paste0("Test ", il$lower," <= ", cls$col_name," < ", eu$upper)
-  class(cls) <- append(class(cls), "inclu_lower_exclu_upper")
+  setup$test_desc <- paste0("Test ", il$lower," <= ", setup$col_name," < ", eu$upper)
+  class(setup) <- append(class(setup), "inclu_lower_exclu_upper")
   
   if (il$test_result & eu$test_result) {
     
-    cls$test_result <- TRUE
-    cls$test_message <- "PASSED"
+    setup$test_result <- TRUE
+    setup$test_message <- "PASSED"
     
   } else {
     
-    cls$test_result <- FALSE
+    setup$test_result <- FALSE
     
     if (il$test_result) {
-      cls$test_message <- paste0("lower bound: ", il$test_message, ", but upper bound ", eu$test_message)
+      setup$test_message <- paste0("lower bound: ", il$test_message, ", but upper bound ", eu$test_message)
     } else if (eu$test_result) {
-      cls$test_message <- paste0("upper bound: ", eu$test_message, ", but lower bound ", il$test_message)
+      setup$test_message <- paste0("upper bound: ", eu$test_message, ", but lower bound ", il$test_message)
     } else {
-      cls$test_message <- paste0("lower bound: ", il$test_message, " and upper bound: ", eu$test_message)
+      setup$test_message <- paste0("lower bound: ", il$test_message, " and upper bound: ", eu$test_message)
     }
   }
-  return(cls)
+  return(setup)
 }
 
 #' @title Test inclu lower and inclu upper
 #' @description Tests that the column is greater than or equal to the lower bound AND less or equal to than the upper bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_inclu_lower_inclu_upper <- function(df, cls) {
+test_inclu_lower_inclu_upper <- function(df, setup) {
   
-  il <- test_inclu_lower(df, cls)
-  iu <- test_inclu_upper(df, cls)
+  il <- test_inclu_lower(df, setup)
+  iu <- test_inclu_upper(df, setup)
   
-  cls$test_desc <- paste0("Test ", il$lower," <= ", cls$col_name," <= ", iu$upper)
-  class(cls) <- append(class(cls), "inclu_lower_inclu_upper")
+  setup$test_desc <- paste0("Test ", il$lower," <= ", setup$col_name," <= ", iu$upper)
+  class(setup) <- append(class(setup), "inclu_lower_inclu_upper")
   
   if (il$test_result & iu$test_result) {
     
-    cls$test_result <- TRUE
-    cls$test_message <- "PASSED"
+    setup$test_result <- TRUE
+    setup$test_message <- "PASSED"
     
   } else {
     
-    cls$test_result <- FALSE
+    setup$test_result <- FALSE
     
     if (il$test_result) {
-      cls$test_message <- paste0("lower bound: ", il$test_message, ", but upper bound ", iu$test_message)
+      setup$test_message <- paste0("lower bound: ", il$test_message, ", but upper bound ", iu$test_message)
     } else if (iu$test_result) {
-      cls$test_message <- paste0("upper bound: ", iu$test_message, ", but lower bound ", il$test_message)
+      setup$test_message <- paste0("upper bound: ", iu$test_message, ", but lower bound ", il$test_message)
     } else {
-      cls$test_message <- paste0("lower bound: ", il$test_message, " and upper bound: ", iu$test_message)
+      setup$test_message <- paste0("lower bound: ", il$test_message, " and upper bound: ", iu$test_message)
     }
   }
-  return(cls)
+  return(setup)
 }
 
 
@@ -239,68 +239,68 @@ test_inclu_lower_inclu_upper <- function(df, cls) {
 #' @description Tests that the column is greater thanto the lower bound AND less or equal to than the upper bound
 #' @inheritParams test_exclu_lower
 #' @export
-test_exclu_lower_inclu_upper <- function(df, cls) {
+test_exclu_lower_inclu_upper <- function(df, setup) {
   
-  el <- test_exclu_lower(df, cls)
-  iu <- test_inclu_upper(df, cls)
+  el <- test_exclu_lower(df, setup)
+  iu <- test_inclu_upper(df, setup)
   
-  cls$test_desc <- paste0("Test ", el$lower," <= ", cls$col_name," <= ", iu$upper)
-  class(cls) <- append(class(cls), "exclu_lower_inclu_upper")
+  setup$test_desc <- paste0("Test ", el$lower," <= ", setup$col_name," <= ", iu$upper)
+  class(setup) <- append(class(setup), "exclu_lower_inclu_upper")
   
   if (el$test_result & iu$test_result) {
     
-    cls$test_result <- TRUE
-    cls$test_message <- "PASSED"
+    setup$test_result <- TRUE
+    setup$test_message <- "PASSED"
     
   } else {
     
-    cls$test_result <- FALSE
+    setup$test_result <- FALSE
     
     if (el$test_result) {
-      cls$test_message <- paste0("lower bound: ", el$test_message, ", but upper bound ", iu$test_message)
+      setup$test_message <- paste0("lower bound: ", el$test_message, ", but upper bound ", iu$test_message)
     } else if (iu$test_result) {
-      cls$test_message <- paste0("upper bound: ", iu$test_message, ", but lower bound ", el$test_message)
+      setup$test_message <- paste0("upper bound: ", iu$test_message, ", but lower bound ", el$test_message)
     } else {
-      cls$test_message <- paste0("lower bound: ", el$test_message, " and upper bound: ", iu$test_message)
+      setup$test_message <- paste0("lower bound: ", el$test_message, " and upper bound: ", iu$test_message)
     }
   }
-  return(cls)
+  return(setup)
 }
 
 
 #' @title Assign the correct numeric test
 #' @description Assigns the correct numeric test depending on the parameters
 #' @inheritParams test_exclu_lower
-test_range <- function(df, cls) {
+test_range <- function(df, setup) {
   
-  if (!is.null(cls$upper) & !is.null(cls$lower)) {
+  if (!is.null(setup$upper) & !is.null(setup$lower)) {
     
-    if (cls$upper_inclu & cls$lower_inclu) {
-      cls <- test_inclu_lower_inclu_upper(df, cls) 
-    } else if (cls$upper_inclu & !cls$lower_inclu) {
-      cls <- test_exclu_lower_inclu_upper(df, cls) 
-    } else if (!cls$upper_inclu & cls$lower_inclu) {
-      cls <- test_inclu_lower_exclu_upper(df, cls)
+    if (setup$upper_inclu & setup$lower_inclu) {
+      setup <- test_inclu_lower_inclu_upper(df, setup) 
+    } else if (setup$upper_inclu & !setup$lower_inclu) {
+      setup <- test_exclu_lower_inclu_upper(df, setup) 
+    } else if (!setup$upper_inclu & setup$lower_inclu) {
+      setup <- test_inclu_lower_exclu_upper(df, setup)
     } else {
-      cls <- test_exclu_lower_exclu_upper(df, cls)
+      setup <- test_exclu_lower_exclu_upper(df, setup)
     }
     
-  } else if (!is.null(cls$upper) & is.null(cls$lower)) {
+  } else if (!is.null(setup$upper) & is.null(setup$lower)) {
     
-     if (cls$upper_inclu) {
-       cls <- test_inclu_upper(df, cls)
+     if (setup$upper_inclu) {
+       setup <- test_inclu_upper(df, setup)
      } else {
-       cls <- test_exclu_upper(df, cls)
+       setup <- test_exclu_upper(df, setup)
      }
     
-  } else if (is.null(cls$upper) & !is.null(cls$lower)) {
+  } else if (is.null(setup$upper) & !is.null(setup$lower)) {
     
-    if (cls$lower_inclu) {
-      cls <- test_inclu_lower(df, cls)
+    if (setup$lower_inclu) {
+      setup <- test_inclu_lower(df, setup)
     } else {
-      cls <- test_exclu_lower(df, cls)
+      setup <- test_exclu_lower(df, setup)
     }
     
   }
-  return(cls)
+  return(setup)
 }
