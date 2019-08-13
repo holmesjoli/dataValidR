038a21cef0374fd2acf46c4ce7b6963ff38448bb
Joli Holmes <jh111@ad.rice.edu>
2019-08-13 11:46:51 -0500

    fixed typo

--- R/apply_tests.R
+++ R/apply_tests.R
@@ -1,55 +1,55 @@
 #' @title Apply test
 #' @export
-apply_test <- function(df, setup, df) {
+apply_test <- function(df, setup) {
   UseMethod("apply_test")
-  
+
 }
 
 apply_test.default <- function(df, setup, ...) {
-  
-  warning(paste("apply_test does not know how to handle object of class ", 
+
+  warning(paste("apply_test does not know how to handle object of class ",
                 class(setup),
                 "and can only be used on classes test_na, test_unique, test_values, and test_range"))
-  
+
 }
 
 #' @title Tests for NAs
 #' @export
 apply_test.test_na <- function(df, setup) {
-  
+
   test <- test_na(df, setup)
   test_result <- test_col(test)
-  
+
   return(test_result)
 }
 
 #' @title Tests for uniqueness
 #' @export
 apply_test.test_unique <- function(df, setup) {
-  
+
   test <- test_unique(df, setup)
   test_result <- test_col(test)
-  
+
   return(test_result)
 }
 
 #' @title Tests for Values
 #' @export
 apply_test.test_values <- function(setup, df) {
-  
+
   test <- test_values(df, setup)
   test_result <- test_col(test)
-  
+
   return(test_result)
 }
 
 #' @title Tests Range of Values
 #' @export
 apply_test.test_range <- function(df, setup) {
-  
+
   test <- test_range(df, setup)
   test_result <- test_col(test)
-  
+
   return(test_result)
 }
 
@@ -59,10 +59,10 @@ apply_test.test_range <- function(df, setup) {
 #' @param setup from the merge setup class
 #' @export
 apply_test.test_one_many <- function(df_left, df_right, setup) {
-  
+
   test <- test_one_many(df_left, df_right, setup)
   test_result <- test_merge(test)
-  
+
   return(test_result)
 }
 
@@ -70,10 +70,10 @@ apply_test.test_one_many <- function(df_left, df_right, setup) {
 #' @inheritParams apply_test.test_one_many
 #' @export
 apply_test.test_one_one <- function(df_left, df_right, setup) {
-  
+
   test <- test_one_one(df_left, df_right, setup)
   test_result <- test_merge(test)
-  
+
   return(test_result)
 }
 
@@ -81,9 +81,9 @@ apply_test.test_one_one <- function(df_left, df_right, setup) {
 #' @inheritParams apply_test.test_one_many
 #' @export
 apply_test.test_many_many <- function(df_left, df_right, setup) {
-  
+
   test <- test_many_many(df_left, df_right, setup)
   test_result <- test_merge(test)
-  
+
   return(test_result)
 }
