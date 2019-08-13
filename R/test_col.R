2f4fc580241f0fc79241fe3ad1288c0376bdf0c5
Joli Holmes <jh111@ad.rice.edu>
2019-08-13 10:35:34 -0500

    replace cls with setup to make it more consistent with example

--- R/test_col.R
+++ R/test_col.R
@@ -1,112 +1,112 @@
 #' @title Test uniqueness
 #' @description Tests if the vector/column is unique
 #' @param df the dataframe
-#' @param cls the setup class for testing
+#' @param setup the setup class for testing
 #' @examples
 #' \dontrun{
 #' df <- data.frame(x = 1:4, y = c(1,1,2:3))
-#' cls <- class_test_unique("df_name", "x", FALSE) 
+#' setup <- class_test_unique("df_name", "x", FALSE) 
 #' 
-#' test <- test_unique(df, cls)
+#' test <- test_unique(df, setup)
 #' ## test$test_result returns TRUE
 #' 
-#' cls <- class_test_unique("df_name", "y", FALSE) 
-#' test <- test_unique(df, cls)
+#' setup <- class_test_unique("df_name", "y", FALSE) 
+#' test <- test_unique(df, setup)
 #' ## test$test_result returns FALSE
 #' }
 #' @export
-test_unique <- function(df, cls) {
+test_unique <- function(df, setup) {
   
-  if (cls$na) {
+  if (setup$na) {
     
-    col <- df[[cls$col_name]][!is.na(df[[cls$col_name]])]
+    col <- df[[setup$col_name]][!is.na(df[[setup$col_name]])]
   } else {
-    col <- df[[cls$col_name]]
+    col <- df[[setup$col_name]]
   }
   
   if (length(unique(col)) == length(col)) {
       
-    cls$test_result <- TRUE
-    cls$test_message <- "PASSED"
+    setup$test_result <- TRUE
+    setup$test_message <- "PASSED"
     
   } else {
-    cls$test_result <- FALSE
-    cls$test_message <- "FAILED: Not Unique"
+    setup$test_result <- FALSE
+    setup$test_message <- "FAILED: Not Unique"
   }
   
-  return(cls)
+  return(setup)
 }
 
 #' @title Test expected values
 #' @description Tests if the vector/column contains values other than expected
 #' @param df the dataframe
-#' @param cls the setup class for testing
+#' @param setup the setup class for testing
 #' @return class with test_result, test_message, test_description
 #' @examples
 #' \dontrun{
 #' df <- data.frame(x = 1:4, y = 5:8)
 #' values <- 1:4
 #' na <- FALSE
-#' cls <- class_test_values("df_name", "x", values, na)
+#' setup <- class_test_values("df_name", "x", values, na)
 #' 
-#' test <- test_values(df, cls)
+#' test <- test_values(df, setup)
 #' ## test$test_result returns TRUE
 #' 
-#' cls <- class_test_values("df_name", "y", values, na)
-#' test <- test_values(df, cls)
+#' setup <- class_test_values("df_name", "y", values, na)
+#' test <- test_values(df, setup)
 #' ## test$test_result returns FALSE
 #' }
 #' @export
-test_values <- function(df, cls) {
+test_values <- function(df, setup) {
   
-  actual_values <- unique(df[[cls$col_name]])
-  add_values <- setdiff(actual_values, cls$values)
+  actual_values <- unique(df[[setup$col_name]])
+  add_values <- setdiff(actual_values, setup$values)
   
   if (length(add_values) > 0) {
     
-    cls$test_result <- FALSE
-    cls$test_message <- paste0("FAILED with additional values in col: ", 
+    setup$test_result <- FALSE
+    setup$test_message <- paste0("FAILED with additional values in col: ", 
                                  paste(add_values, collapse = ","))
     
   } else {
    
-    cls$test_result <- TRUE
-    cls$test_message <- "PASSED"
+    setup$test_result <- TRUE
+    setup$test_message <- "PASSED"
     
   }
   
-  return(cls)
+  return(setup)
 }
 
 
 #' @title Test NA values
 #' @description Tests if the vector/column contains any NA values
 #' @param df the dataframe
-#' @param cls the setup class for testing
+#' @param setup the setup class for testing
 #' @examples
 #' \dontrun{
 #' df <- data.frame(x = 1:4, y = c(NA, 6:8))
-#' cls <- class_test_na("df_name", "x") 
-#' test <- test_na(df, cls)
+#' setup <- class_test_na("df_name", "x") 
+#' test <- test_na(df, setup)
 #' ## test$test_result returns TRUE
 #' 
-#' cls <- class_test_na("df_name", "y") 
-#' test <- test_na(df, cls)
+#' setup <- class_test_na("df_name", "y") 
+#' test <- test_na(df, setup)
 #' ## test$test_result returns FALSE
 #' }
 #' @export
-test_na <- function(df, cls) {
+test_na <- function(df, setup) {
   
-    if (all(!is.na(df[[cls$col_name]]))) {
+    if (all(!is.na(df[[setup$col_name]]))) {
         
-      cls$test_result <- TRUE
-      cls$test_message <- "PASSED"
+      setup$test_result <- TRUE
+      setup$test_message <- "PASSED"
       
     } else {
       
-      cls$test_result <- FALSE  
-      cls$test_message <- "FAILED: Contains na values"
+      setup$test_result <- FALSE  
+      setup$test_message <- "FAILED: Contains na values"
     }
     
-  return(cls)
+  return(setup)
 }
